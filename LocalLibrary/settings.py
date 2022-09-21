8e9dbe40f671002ae82ed625adfce16a13835b02
Vladislav <89801358+VladYura@users.noreply.github.com>
2022-09-21 21:22:58 +0300

    Update settings.py

--- LocalLibrary/settings.py
+++ LocalLibrary/settings.py
@@ -125,3 +125,8 @@ STATICFILES_DIRS = [STATIC_DIR]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
